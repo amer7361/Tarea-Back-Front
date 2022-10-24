a7442b26fecc3b7a2d64a5b1c4ff35f8a8ff90c9
amer7361 <erikitagonzalez2001@gmail.com>
2022-10-23 19:55:35 -0600

    Tarea Finalizada

---
+++ Api_Back/Controllers/EmpleadosController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TareaBackFront.Models;
+
+namespace TareaBackFront.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmpleadosController : ControllerBase
+    {
+        private readonly dbempleadoContext _context;
+
+        public EmpleadosController(dbempleadoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Empleados
+        [HttpGet]
+        public async Task<ActionResult> GetEmpleados()
+        {
+            var listaEmpleados = await _context.Empleados.Join(_context.Puestos,
+                emp=> emp.IdPuesto,
+                puest=> puest.IdPuesto,
+                (emp, puest) => new
+                {
+                   IdEmpleado = emp.IdEmpleado,
+                   Nombre = emp.Nombre,
+                   Apellido = emp.Apellido,
+                   Direccion = emp.Direccion,
+                   Telefono = emp.Telefono,
+                   IdPuesto = puest.IdPuesto,
+                   Dpi = emp.Dpi,
+                   FechaNacimiento = emp.FechaNacimiento,
+                   FechaIngresoRegresitro = emp.FechaIngresoRegresitro,
+                   Puesto = puest.Puesto1
+                }).ToListAsync();
+            return Ok(listaEmpleados);
+        }
+
+        // GET: api/Empleados/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Empleado>> GetEmpleado(int id)
+        {
+            var empleado = await _context.Empleados.FindAsync(id);
+
+            if (empleado == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(empleado);
+        }
+
+        // PUT: api/Empleados/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEmpleado(int id, Empleado empleado)
+        {
+            if (id != empleado.IdEmpleado)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(empleado).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmpleadoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Empleados
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Empleado>> PostEmpleado(Empleado empleado)
+        {
+            _context.Empleados.Add(empleado);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetEmpleado", new { id = empleado.IdEmpleado }, empleado);
+        }
+
+        // DELETE: api/Empleados/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmpleado(int id)
+        {
+            var empleado = await _context.Empleados.FindAsync(id);
+            if (empleado == null)
+            {
+                return NotFound();
+            }
+
+            _context.Empleados.Remove(empleado);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool EmpleadoExists(int id)
+        {
+            return _context.Empleados.Any(e => e.IdEmpleado == id);
+        }
+    }
+}
